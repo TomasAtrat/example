6c59f555eafb927c9c3c3e9c7c8420966c37eeac
start.vaadin.com <no-reply@vaadin.com>
2022-01-26 22:12:36 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/configuration/ConfigurationView.java
@@ -0,0 +1,31 @@
+package com.example.application.views.configuration;
+
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("Configuration")
+@Route(value = "configuration", layout = MainLayout.class)
+public class ConfigurationView extends HorizontalLayout {
+
+    private TextField name;
+    private Button sayHello;
+
+    public ConfigurationView() {
+        name = new TextField("Your name");
+        sayHello = new Button("Say hello");
+        sayHello.addClickListener(e -> {
+            Notification.show("Hello " + name.getValue());
+        });
+
+        setMargin(true);
+        setVerticalComponentAlignment(Alignment.END, name, sayHello);
+
+        add(name, sayHello);
+    }
+
+}
